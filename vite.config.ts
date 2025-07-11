f3c430116e172f46ae6a8ecf88b0ad77cf52bcca
Harshad Dhumane <64719740+harshad-Dhumane@users.noreply.github.com>
2025-07-11 19:12:06 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/snap-domain-showcase/' : '/',
+  base: mode === 'production' ? '/llmsnap/' : '/',
   server: {
     host: "::",
     port: 8080,
